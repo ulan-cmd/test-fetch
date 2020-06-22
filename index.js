aea24c6cb302219c771f82d5dc26391a1ee1b260
ulan-cmd <ulan22.work@gmail.com>
2020-06-22 21:08:43 +0600

    init: project

---
+++ index.js
@@ -0,0 +1,22 @@
+//GET запрос
+// const url = 'https://jsonplaceholder.typicode.com/users';
+
+// fetch(url)
+//  .then(response => response.json())
+//  .then(data => show_users(data));
+
+
+ // function show_users(data) {
+ // 	let out = '';
+ // 	for(let item of data){
+ // 		out += `${item.id} - ${item.name}`;
+ // 		out += '<br>';
+ // 	}
+ // 	document.querySelector('body').innerHTML = out;
+ // }
+ // id
+ // name
+ // username
+ // email
+ // phone
+ // website
